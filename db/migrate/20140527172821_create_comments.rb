df5fc966c56c96316c092f8d12af33bbcd71dcab
deepak <deepak24chopra@gmail.com>
2014-05-28 23:25:23 +0530

    'comm'

--- db/migrate/20140527172821_create_comments.rb
+++ db/migrate/20140527172821_create_comments.rb
@@ -1,11 +1,11 @@
 class CreateComments < ActiveRecord::Migration
   def change
-    #create_table :comments, :force => true do |t|
-     # t.string :username
-     # t.integer :episode_id
-      #t.text :content
+    create_table :comments, :force => true do |t|
+      t.string :username
+      t.integer :episode_id
+      t.text :content
 
-     # t.timestamps
-    #end
+      t.timestamps
+    end
   end
 end
